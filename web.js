477565996fcb02add7ac0b9bb6360d83c8593c35
Radley Obligacion <oradley@gmail.com>
2013-07-12 02:58:52 +0000

    Updated web.js to read index.html

--- web.js
+++ web.js
@@ -1,10 +1,18 @@
 var express = require('express');
 
+var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+fs.readFile('index.html', function (err, data) {
+    if (err) throw err;
+    console.log(data);
+    response.send(data);
 });
+
+//app.get('/', function(request, response) {
+//  response.send('Hello World 2!');
+//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
